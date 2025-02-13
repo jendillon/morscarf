9ee87dc918729e925308a9788b8c4926d7dccba9
Jennifer Dillon <jen.j.dillon@gmail.com>
2025-02-12 18:39:37 -0800

    Changing to flask and python in a web app

---
+++ app.py
@@ -0,0 +1,71 @@
+import io
+from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
+import matplotlib
+import base64
+from io import BytesIO
+import numpy as np
+
+matplotlib.use('Agg')  # Required to use Matplotlib without X server
+app = Flask(__name__)
+
+MORSE_CODE_DICT = {
+    'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',    
+    'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---', 
+    'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---',  
+    'P': '.--.',  'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',    
+    'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',  'Y': '-.--', 
+    'Z': '--..',  '0': '-----',  '1': '.----', '2': '..---', '3': '...--', 
+    '4': '....-',  '5': '.....', '6': '-....', '7': '--...', '8': '---..', 
+    '9': '----.'
+}
+
+def text_to_morse(text):
+    text = text.upper()
+    morse_code = []
+    for char in text:
+        if char in MORSE_CODE_DICT:
+            morse_code.append(MORSE_CODE_DICT[char])
+        elif char == ' ':
+            morse_code.append(' ')  # Space between words
+    return ' '.join(morse_code)
+
+def morse_to_stripes(morse_code):
+    stripes = np.array([])
+    for symbol in morse_code:
+        if symbol == '.':
+            stripes = np.append(stripes, 1) # Skinny stripe
+        elif symbol == '-':
+            stripes = np.append(stripes, [1, 1, 1])  # Wide stripe
+        elif symbol == ' ':
+            stripes = np.append(stripes, 0) # Space between words
+    print(stripes)
+    return stripes
+
+def visualize_stripes(stripes):
+    pixels_per_stripe = 4
+    dpi = 100
+
+    fig = plt.figure(figsize=(len(stripes)*pixels_per_stripe/dpi, 2), dpi=dpi)
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.set_axis_off()
+    ax.imshow(stripes.reshape(1, -1), cmap='binary', aspect='auto', interpolation='nearest')
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return plot_url
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    
+    if request.method == "POST":
+        phrase = request.form['phrase']
+        morse_code = text_to_morse(phrase)
+        stripes = morse_to_stripes(morse_code)
+        plot_url = visualize_stripes(stripes)
+        return render_template("index.html", morse_code=morse_code, plot_url=plot_url)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
